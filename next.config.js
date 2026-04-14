40e0c349954b9f5e7f66352c9434da61bc217f0c
Veeresh R <veeresh.renukaradhya@tarento.com>
2026-04-14 12:10:52 +0530

    clean deploy: only out folder for github pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
   basePath: '/DreamDesignerWebsite',
-
   trailingSlash: true,
-
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
