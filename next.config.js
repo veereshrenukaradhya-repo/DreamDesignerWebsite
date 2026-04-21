5b6315f92b663a03daa0f468ad698d8f3228935e
Veeresh R <veeresh.renukaradhya@tarento.com>
2026-04-21 17:46:26 +0530

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   output: "export",
-  basePath: "/DreamDesignerWebsite",   // ✅ NO trailing slash
-  assetPrefix: "/DreamDesignerWebsite/", // keep slash here
+  basePath: "/DreamDesignerWebsite",   // no trailing slash
+  assetPrefix: "/DreamDesignerWebsite/", // keep trailing slash
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
