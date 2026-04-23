30c2b7bd03730c38efd0be41cb8ad0a1b560a159
Veeresh R <veeresh.renukaradhya@tarento.com>
2026-04-23 18:31:59 +0530

    fix css loading via base href

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-
-  basePath: isProd ? "/DreamDesignerWebsite" : "",
-  assetPrefix: isProd ? "/DreamDesignerWebsite/" : "",
-
   trailingSlash: true,
-
   images: {
     unoptimized: true,
   },
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
-import "../styles/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <base href="/DreamDesignerWebsite/" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
