5b163c516de815095852d89740787e97d60c02cd
Veeresh R <veeresh.renukaradhya@tarento.com>
2026-04-23 17:56:35 +0530

    hard reset tailwind fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
-module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
